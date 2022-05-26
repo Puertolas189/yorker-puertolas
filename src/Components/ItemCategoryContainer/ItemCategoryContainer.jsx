6c4379b1a43be2c6a51a5884364bd15e550b1cc6
Daniel Puértolas <puertolas189@hotmail.com>
2022-05-26 14:22:36 -0300

    Fix

--- src/Components/ItemCategoryContainer/ItemCategoryContainer.jsx
+++ src/Components/ItemCategoryContainer/ItemCategoryContainer.jsx
@@ -2,48 +2,32 @@ import React, { useEffect, useState } from "react";
 import { data } from "../../Config/Data";
 import { useParams } from "react-router-dom";
 import ItemList from "../ItemList/ItemList";
+import { collection, getDocs } from "firebase/firestore";
 import db from "../../Service/firebase";
-import { getDocs, collection, query, where } from "firebase/firestore";
 
 function ItemCategoryContainer() {
   const { id } = useParams();
   const [productos, setProductos] = useState([]);
-  const filtro = data.filter((prod) => prod.categoria === Number(id));
-
-  // useEffect(() => {
-  //   const detalle = new Promise((resolve, reject) => {
-  //       resolve(filtro);
-  //   });
-  //   detalle
-  //     .then((res) => {
-  //       setProductos(res);
-  //     })
-  //     .catch((err) => console.log(err));
-
-  //   return () => {setProductos([])};
-  // }, [id]);
 
   const getData = async () => {
-    console.log(id);
-    const docs = query(
-      collection(db, "Productos"),
-      where("categoria", "==", id)
-    );
-    console.log(docs);
+    const docs = collection(db, "Productos");
+
     try {
-      getDocs(docs).then((snapshot) => {
-        if (snapshot.size === 0) {
-          console.log("Sin resultados");
-        }
-        setProductos(
-          snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
-        );
-      });
+      const datos = await getDocs(docs);
+      const result = datos.docs.map(
+        (doc) => (doc = { id: doc.id, ...doc.data() })
+      );
+      const filtro = result.filter((prod) => prod.categoria === Number(id));
+      setProductos(filtro);
+      
     } catch (error) {}
   };
   useEffect(() => {
     getData();
-  }, []);
+    return () => {
+      setProductos([]);
+    };
+  }, [id]);
 
   return (
     <div style={{ align: "center" }}>
